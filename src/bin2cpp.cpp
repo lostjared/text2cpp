53f2ded47a6ee5dbe5b96fd7bfe3675ee1d7480f
lostjared <jaredbruni@gmail.com>
2017-08-19 11:06:36 -0700

    updated to now output go

--- src/bin2cpp.cpp
+++ src/bin2cpp.cpp
@@ -50,6 +50,35 @@ void outputCpp17(std::string &filename, std::fstream &file, std::string &filen, 
     std::cout << "Outputed: " << filename << "\n";
 }
 
+void outputGo(std::string &filename, std::fstream &file, std::string &filen, std::fstream &outfile) {
+
+    outfile << "package " << filen << "\n\n\n";
+    file.seekg(0, std::ios::end);
+    size_t len = file.tellg();
+    file.seekg(0, std::ios::beg);
+    
+    outfile << "func Create_" << filen << "() []uint8 {\n\n";
+    outfile << "value := []uint8 {\n";
+    
+    unsigned long counter = 0;
+    char buffer[1024];
+    while(!file.eof()) {
+        file.read((char*)buffer, 1024);
+        for(unsigned int i = 0; i < file.gcount(); ++i) {
+            outfile << "0x" << std::hex << (unsigned int)(unsigned char)buffer[i] << ", ";
+            ++counter;
+            if((counter%15) == 0) outfile << "\n";
+            
+        }
+    }
+    file.close();
+    outfile << "\n0x0" << "}\n\n\n";
+    outfile << "return value\n}\n\n";
+    outfile.close();
+    std::cout << "Array Variable: " << filen << "\n";
+    std::cout << "Outputed: " << filename << "\n";
+}
+
 void outputCpp(std::string &filename, std::fstream &file, std::string &filen, std::fstream &outfile, std::string &arg_x, char **argv) {
     outfile << "#ifndef " << filen << "__H\n";
     outfile << "#define " << filen << "__H\n\n";
@@ -103,14 +132,11 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
     std::string arg_x = argv[3];
-    
-    if(arg_x != "c" && arg_x != "cpp" && arg_x != "cpp17") {
-        std::cerr << "Invalid language selection please use either c or cpp or cpp17\n";
+    if(arg_x != "c" && arg_x != "cpp" && arg_x != "cpp17" && arg_x != "go") {
+        std::cerr << "Invalid language selection please use either c or cpp or cpp17 or go\n";
         exit(EXIT_FAILURE);
     }
-    
     std::string filen = convertFilename(argv[1]);
-    
     std::fstream file;
     file.open(argv[1], std::ios::in | std::ios::binary);
     if(!file.is_open()) {
@@ -118,7 +144,13 @@ int main(int argc, char **argv) {
         return 0;
     }
     std::string filename=argv[2];
-    filename += ".h";
+    if(arg_x == "c" || arg_x == "cpp")
+    	filename += ".h";
+    else if(arg_x == "cpp17")
+        filename += ".hpp";
+    else if(arg_x == "go")
+        filename += ".go";
+    
     std::fstream outfile;
     outfile.open(filename, std::ios::out);
     if(!outfile.is_open()) {
@@ -130,8 +162,11 @@ int main(int argc, char **argv) {
     if(arg_x == "cpp17") {
         outputCpp17(filename, file, filen, outfile);
     }
-    else {
+    else if(arg_x == "c" || arg_x == "cpp") {
         outputCpp(filename, file, filen, outfile, arg_x, argv);
+    }
+    else if(arg_x == "go") {
+        outputGo(filename, file, filen, outfile);
     }
     return 0;
 }
