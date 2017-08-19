074a51bd94eb0a170ac5c4c82c5fb69b49d6531a
lostjared <jaredbruni@gmail.com>
2017-08-19 11:14:51 -0700

    example source files

---
+++ examples/c_example.h
@@ -0,0 +1,10 @@
+#ifndef bin2cpp_cpp__H
+#define bin2cpp_cpp__H
+
+extern unsigned char bin2cpp_cpp[];
+extern unsigned long bin2cpp_cpp_length;
+
+
+
+
+#endif
---
+++ examples/cppSource.h
@@ -0,0 +1,17 @@
+#ifndef bin2cpp_cpp__H
+#define bin2cpp_cpp__H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern unsigned char bin2cpp_cpp[];
+extern unsigned long bin2cpp_cpp_length;
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif
